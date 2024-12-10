63dded2a3d57729b1e677a9ceeb2b56b47f89580
ducanh <ducanh070801kk@gmail.com>
2024-12-10 17:22:55 +0700

    ducanh thay doi

--- src/pages/Products/product.jsx
+++ src/pages/Products/product.jsx
@@ -17,6 +17,8 @@ import apiCaller from "../../api/apiCaller";
 import { productApi } from "../../api/productApi";
 import { setSortProduct } from "../../features/productSlice";
 import { Line } from "react-chartjs-2";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Flex, Spin } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 export default function ProductScreen() {
   const location = useLocation();
@@ -25,7 +27,15 @@ export default function ProductScreen() {
   const [pageCount, setPageCount] = useState(1);
   const queryParams = new URLSearchParams(location.search);
   const category_id = queryParams.get("category_id");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoadingStates] = useState({
+    page:true,
+    content:true,
+    priceHistory: true,
+    relatedProducts: true,
+  });;
+  const setLoading = (key, value) => {
+    setLoadingStates((prev) => ({ ...prev, [key]: value }));
+  };
   const [productDetail, setProductDetail] = useState({});
   const [productRelate, setProductRelate] = useState([]);
   const [modal, setModal] = useState({ open: false, product_id: null });
@@ -107,126 +117,149 @@ export default function ProductScreen() {
     {
       key: "1",
       label: "Lịch sử giá",
-      children: <Line options={options} data={dataDetail} />,
+      children: loading.priceHistory ? (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "200px", // Chiều cao đủ để hiển thị spinner
+        }}
+      >
+        <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+      </div>
+    ) : (
+      <Line options={options} data={dataDetail} />
+    ),
     },
     {
       key: "2",
       label: "Sản phẩm liên quan",
-      children:
-        productRelate.length > 0 ? (
-          <Carousel
-            slidesToShow={3}
-            infinite={true}
-            arrows
-            dots={false}
-            draggable={true}
-            className={s.carousel}
-          >
-            {productRelate?.map((val, index) => (
-              <div
-                key={index}
-                className={s.image}
-                style={{ cursor: "default", maxWidth: "200px" }}
-              >
-                <img
-                  style={{ borderRadius: "10px 10px 0 0" }}
-                  src={val.image_url}
-                  alt="image"
-                />
-                <hr
-                  style={{
-                    width: "100%",
-                    color: "black",
-                    marginTop: "0",
-                  }}
-                />
-                <div>
-                  <Tooltip title={val.product_name}>
-                    <p className={s.text_clamp}>{val.product_name}</p>
-                  </Tooltip>
-                  <div className={s.content1}>
-                    <p
-                      style={{
-                        textDecoration: "line-through",
-                        color: "#828282",
-                        fontWeight: 600,
-                        fontSize: 12,
-                      }}
-                    >
-                      {val.origin_price?.toLocaleString("vi-VN")}VNĐ
-                    </p>
-                    <p
-                      style={{
-                        color: "red",
-                        fontWeight: 700,
-                        fontSize: 18,
-                      }}
-                    >
-                      {val.price?.toLocaleString("vi-VN")}VNĐ
-                    </p>
-                  </div>
-                  <div className={s.content2}>
-                    <p>
-                      Đã bán{" "}
-                      {val.all_time_quantity_sold?.toLocaleString("vi-VN")}
-                    </p>
-                    <p
-                      style={{
-                        display: "flex",
-                        gap: "3px",
-                        flexDirection: "row-reverse",
-                      }}
-                    >
-                      <img src="/star.svg" alt="star" />
-                      {val.rating_average}
-                    </p>
-                  </div>
-                  <p className={s.discount}>-{val.discount_rate}%</p>
-                  <div
+      children: loading.relatedProducts ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "300px", // Chiều cao đủ để hiển thị spinner
+          }}
+        >
+          <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+        </div>
+      ) : productRelate.length > 0 ? (
+        <Carousel
+          slidesToShow={3}
+          infinite={true}
+          arrows
+          dots={false}
+          draggable={true}
+          className={s.carousel}
+        >
+          {productRelate?.map((val, index) => (
+            <div
+              key={index}
+              className={s.image}
+              style={{ cursor: "default", maxWidth: "200px" }}
+            >
+              <img
+                style={{ borderRadius: "10px 10px 0 0" }}
+                src={val.image_url}
+                alt="image"
+              />
+              <hr
+                style={{
+                  width: "100%",
+                  color: "black",
+                  marginTop: "0",
+                }}
+              />
+              <div>
+                <Tooltip title={val.product_name}>
+                  <p className={s.text_clamp}>{val.product_name}</p>
+                </Tooltip>
+                <div className={s.content1}>
+                  <p
                     style={{
-                      display: "grid",
-                      gridTemplateColumns: "repeat(10,1fr)",
+                      textDecoration: "line-through",
+                      color: "#828282",
+                      fontWeight: 600,
+                      fontSize: 12,
                     }}
                   >
-                    <Link
-                      target="_blank"
-                      className={s.btn}
-                      onClick={(e) => {
-                        e.stopPropagation();
+                    {val.origin_price?.toLocaleString("vi-VN")}VNĐ
+                  </p>
+                  <p
+                    style={{
+                      color: "red",
+                      fontWeight: 700,
+                      fontSize: 18,
+                    }}
+                  >
+                    {val.price?.toLocaleString("vi-VN")}VNĐ
+                  </p>
+                </div>
+                <div className={s.content2}>
+                  <p>
+                    Đã bán{" "}
+                    {val.all_time_quantity_sold?.toLocaleString("vi-VN")}
+                  </p>
+                  <p
+                    style={{
+                      display: "flex",
+                      gap: "3px",
+                      flexDirection: "row-reverse",
+                    }}
+                  >
+                    <img src="/star.svg" alt="star" />
+                    {val.rating_average}
+                  </p>
+                </div>
+                <p className={s.discount}>-{val.discount_rate}%</p>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(10,1fr)",
+                  }}
+                >
+                  <Link
+                    target="_blank"
+                    className={s.btn}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                    to={val.url_path}
+                  >
+                    Tới nơi bán
+                  </Link>
+                  {val.category_id.includes("s") ? (
+                    <img
+                      style={{
+                        gridColumn: "span 3",
+                        width: "100%",
+                        borderRadius: "0 0 10px 0",
                       }}
-                      to={val.url_path}
-                    >
-                      Tới nơi bán
-                    </Link>
-                    {val.category_id.includes("s") ? (
-                      <img
-                        style={{
-                          gridColumn: "span 3",
-                          width: "100%",
-                          borderRadius: "0 0 10px 0",
-                        }}
-                        alt="tiki"
-                        src="/sendo.svg"
-                      />
-                    ) : (
-                      <img
-                        style={{
-                          gridColumn: "span 3",
-                          width: "100%",
-                          borderRadius: "0 0 10px 0",
-                        }}
-                        alt="tiki"
-                        src="/tiki.svg"
-                      />
-                    )}
-                  </div>
+                      alt="sendo"
+                      src="/sendo.svg"
+                    />
+                  ) : (
+                    <img
+                      style={{
+                        gridColumn: "span 3",
+                        width: "100%",
+                        borderRadius: "0 0 10px 0",
+                      }}
+                      alt="tiki"
+                      src="/tiki.svg"
+                    />
+                  )}
                 </div>
               </div>
-            ))}
-          </Carousel>
-        ) : (
-          <div>Không có sản phẩm liên quan</div>
-        ),
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <div>Không có sản phẩm liên quan</div>
+      ),
     },
   ];
   const {
@@ -280,13 +313,15 @@ export default function ProductScreen() {
   useEffect(() => {
     if (category_id) {
       dispatch(setSortProduct({ category_id: category_id }));
+      setLoading("content", false);
     }
   }, [category_id]);
   useEffect(() => {
     if (sortProduct.category_id) {
       fetchListProduct(sortProduct).then((res) => {
         setDataList(res.data);
-        setLoading(false);
+        setLoading("page",false);
+        setLoading("content",false);
       });
     }
   }, [sortProduct]);
@@ -294,15 +329,19 @@ export default function ProductScreen() {
     if (modal.product_id != null) {
       fetchProductDetail({ product_id: modal.product_id }).then((res) => {
         setProductDetail(res.data);
+        setLoading("priceHistory", false);
       });
       fetchProductRelate({ product_id: modal.product_id }).then((res) => {
         setProductRelate(res.data);
+        setLoading("relatedProducts", false);
       });
     }
   }, [modal]);
   const onClick = (e) => {
+    setLoading("content", true);
     navigate(`/products?category_id=${e.key}`);
     dispatch(setSortProduct({ category_id: e.key }));
+    
   };
   const itemSort2 = [
     {
@@ -310,6 +349,7 @@ export default function ProductScreen() {
       label: (
         <p
           onClick={() => {
+            setLoading("content", true);
             dispatch(
               setSortProduct({
                 sort_by: "all_time_quantity_sold",
@@ -328,6 +368,7 @@ export default function ProductScreen() {
       label: (
         <p
           onClick={() => {
+            setLoading("content", true);
             dispatch(
               setSortProduct({
                 sort_by: "all_time_quantity_sold",
@@ -348,6 +389,7 @@ export default function ProductScreen() {
       label: (
         <p
           onClick={() => {
+            setLoading("content", true);
             dispatch(
               setSortProduct({
                 sort_by: "price",
@@ -366,6 +408,7 @@ export default function ProductScreen() {
       label: (
         <p
           onClick={() => {
+            setLoading("content", true);
             dispatch(
               setSortProduct({
                 sort_by: "price",
@@ -383,8 +426,16 @@ export default function ProductScreen() {
 
   return (
     <>
-      {loading ? (
-        <p>loading...</p>
+      {loading.page ? (
+        <Flex style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Spin indicator={<LoadingOutlined style={{ fontSize: 100 }} spin />} />
+        </Flex>
       ) : (
         <Layout hasSider>
           <Sider style={siderStyle}>
@@ -420,7 +471,7 @@ export default function ProductScreen() {
                       height: "100%",
                       borderRadius: "0 0 10px 0",
                     }}
-                    alt="tiki"
+                    alt="sendo"
                     src="/sendo.svg"
                   />
                 ) : (
@@ -460,6 +511,17 @@ export default function ProductScreen() {
                 overflow: "initial",
               }}
             >
+              {loading.content ? (
+                <Flex style={{
+                  width: "100vw",
+                  height: "100vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>
+                  <Spin indicator={<LoadingOutlined style={{ fontSize: 100 }} spin />} />
+                </Flex>
+              ) : (
               <div
                 style={{
                   padding: 24,
@@ -474,6 +536,8 @@ export default function ProductScreen() {
                       className={s.image}
                       onClick={(e) => {
                         e.stopPropagation();
+                        setLoading("priceHistory", true);
+                        setLoading("relatedProducts", true);
                         setModal({
                           open: true,
                           product_id: val.product_id,
@@ -589,6 +653,7 @@ export default function ProductScreen() {
                   showSizeChanger={false}
                 />
               </div>
+              )}
             </Content>
           </Layout>
         </Layout>
